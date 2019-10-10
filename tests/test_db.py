98a7722213404ae32aa74f5298a58722842d466f
Pavel Suchman <pavels@datascience.co.il>
2019-10-10 16:49:39 +0300

    Rename src directory to infra

--- tests/test_db.py
+++ tests/test_db.py
@@ -5,7 +5,7 @@ import pytest
 from pymongo import MongoClient
 from pymongo.database import Database
 
-from src.db import MongoHandler
+from infra.db import MongoHandler
 
 
 @pytest.fixture(scope='module')
