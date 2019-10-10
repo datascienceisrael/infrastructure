49b471f416dc0c96d49cc50464dfd72332847298
spinelsun <or@datascience.co.il>
2019-10-10 18:55:08 +0300

    fix the enum support for different python versions

--- infra/enums.py
+++ infra/enums.py
@@ -1,4 +1,10 @@
-from enum import Enum, auto
+import sys
+python_version = str(sys.version_info[0]) + str(sys.version_info[1])
+
+if python_version >= 3.6:
+    from enum import Enum, auto
+else:
+    from aenum import Enum, auto
 
 class LogSeverities(Enum):
     DEBUG = auto()
