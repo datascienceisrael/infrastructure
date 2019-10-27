685149a982c8748f2de2b22dcdab71b1285257eb
spinelsun <or@datascience.co.il>
2019-10-27 17:01:18 +0200

    supporting environment for logs

--- infra/logging.py
+++ infra/logging.py
@@ -1,6 +1,6 @@
 import google.cloud.logging as gcl
 
-from infra.enums import LogSeverities
+from infra.enums import LogSeverities, Environments
 
 _stackdriver_client = gcl.Client()
 
@@ -17,6 +17,7 @@ def gcl_delete_logs(logger_name: str):
 
 def gcl_log_event(logger_name: str, event_name: str,  message: str,
                   description: str = None,
+                  environment: Environments = Environments.DEV,
                   severity: LogSeverities = LogSeverities.INFO,
                   **kwargs):
     """Log an event to Google Cloud Logging (Stackdrive).
@@ -26,12 +27,19 @@ def gcl_log_event(logger_name: str, event_name: str,  message: str,
         event_name (str): The event name.
         message (str): The event message.
         description (str): The event description.
-        severity (LogSeverities): The severity of the event.
+        environment (Environments): The environment that the longs belong to.
+        Defaults to DEV
+        severity (LogSeverities): The severity of the event. Defaults to INFO
         **kwargs: Any other metadata on the event.
     """
     logger = _stackdriver_client.logger(logger_name)
 
-    info = {'message': message, 'name': event_name, 'description': description}
+    info = {
+        'message': message,
+        'name': event_name,
+        'description': description,
+        'env': environment.name.lower()
+    }
     info.update(kwargs)
     logger.log_struct(info, severity=severity.name)
 
