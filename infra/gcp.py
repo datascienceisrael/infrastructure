d3421db3f6933fc3d8f77a249806de64032b906b
spinelsun <or@datascience.co.il>
2019-10-29 08:49:02 +0200

    finish implementing save artifact function improvments to create bucket function

--- infra/gcp.py
+++ infra/gcp.py
@@ -1,55 +1,128 @@
+from typing import Any, Dict
+
 from google.cloud import storage
-from google.cloud.exceptions import Conflict
+from google.cloud.exceptions import Conflict, GoogleCloudError, NotFound
 
 from configuration import config
-from infra.enums import LogSeverities
+from infra.enums import Environments, LogSeverities, StorageClasses
 from infra.logging import gcl_log_event
 
 _gcs_client = storage.Client()
 
 
 def create_bucket(bucket_name: str, app_name: str,
-                  storage_class: str = "STANDARD",
-                  logger_name: str = config.LOGGER_NAME):
+                  storage_class: StorageClasses = StorageClasses.STANDARD,
+                  logger_name: str = config.LOGGER_NAME) -> bool:
     """
-    Create a new bucket in Google Cloud Storage.
+     Create a new bucket in Google Cloud Storage.
 
-    Args:
-        bucket_name (str): The name of the bucket.
-        app_name (str): The name of the application the use this function.
-        This name is used to create a unique name for the bucket.
-        storage_class (str): The [storage class](https://cloud.google.com/storage/docs/storage-classes)
-        of the bucket.
-        Possible options are: STANDARD, NEARLINE, COLDLINE, MULTI_REGIONAL,
-        REGIONAL, DURABLE_REDUCED_AVAILABILITY. Defaults to STANDARD.
-        logger_name (str, optional): The name of the logger that logs the
-        event. Defaults to 'infra'.
+     Args:
+         bucket_name (str): The name of the bucket.
+         app_name (str): The name of the application the use this function.
+         This name is used to create a unique name for the bucket.
+         storage_class (str): The storage class of the bucket.
+         Possible options are: STANDARD, NEARLINE, COLDLINE.
+         Defaults to StorageClasses.STANDARD.
+         logger_name (str, optional): The name of the logger that logs the
+         event. Defaults to 'infra'.
 
-    Returns:
-        bool: True if the bucket was created, false otherwise.
-    """
+     Returns:
+         bool: True if the bucket was created, false otherwise.
+     """
     unique_name = app_name + '_' + bucket_name
+    metadata = {
+        'bucketName': bucket_name,
+        'funcName': 'create_bucket',
+        'eventGroup': 'Google Cloud Storage'
+    }
+
     try:
         new_bucket = _gcs_client.bucket(unique_name)
-        new_bucket.storage_class(storage_class.upper())
+        new_bucket.storage_class(storage_class.name.upper())
         _gcs_client.create_bucket(new_bucket)
         gcl_log_event(logger_name=logger_name,
                       event_name='Bucket Created',
                       message='A new bucket was created',
-                      bucket_name=bucket_name,
-                      func_name='create_bucket',
-                      event_group="Google Cloud Storage")
+                      environment=Environments.INFRA,
+                      **metadata)
 
         return True
     except Conflict as ce:
         gcl_log_event(logger_name=logger_name,
                       event_name='Bucket Error',
                       message=str(ce),
+                      environment=Environments.INFRA,
                       severity=LogSeverities.WARNING,
-                      bucket_name=bucket_name,
-                      func_name="create_bucket",
-                      event_group="Google Cloud Storage")
+                      **metadata)
 
+        return False
+
+
+def save_artifact(bucket_name: str,
+                  blob_name: str,
+                  file_path: str,
+                  metadata: Dict[str, Any] = None,
+                  logger_name: str = config.LOGGER_NAME) -> bool:
+    """
+    Save an artifact to Google Cloud Storage under.
+    An "artifact" can be any type of file in any size.
+    Each artifact can be saved with its own metadata.
+
+    Args:
+        bucket_name (str): The bucket that contains the artifact.
+        blob_name (str): A 'blob' is a place holder for the file itself.
+        In general the blob name is the path of the artifact inside GCS.
+        It can be a directory-like name (e.g my/gcp/blob) or a file-like name
+        (e.g my_blob).
+        artifact_path (str): The path to the file under GCS.
+        metadata (Dict[str, Any], optional): [description]. Defaults to None.
+        logger_name (str):  The name of the logger that logs the
+        event. Defaults to 'infra'.
+
+    Returns:
+         bool: True if the file was uploaded, false otherwise.
+    """
+    log_metadata = {
+        'funcName': 'save_artifact',
+        'eventGroup': 'Google Cloud Storage'
+    }
+
+    try:
+        bucket = _gcs_client.get_bucket(bucket_name)
+        blob = bucket.blob(blob_name)
+        blob.metadata(metadata)
+
+        with open(file_path, 'rb') as f:
+            blob.upload_from_file(f)
+
+        return True
+    except NotFound as nfe:
+        msg = 'The requested bucket was not found.'
+        gcl_log_event(logger_name=logger_name,
+                      event_name='Artifact Uploading Error',
+                      message=msg,
+                      description=str(nfe),
+                      environment=Environments.INFRA,
+                      bucketName=bucket_name,
+                      **log_metadata)
+        return False
+    except GoogleCloudError as gce:
+        msg = 'An error accrued while trying to upload the file.'
+        gcl_log_event(logger_name=logger_name,
+                      event_name='Artifact Uploading Error',
+                      message=msg,
+                      description=str(gce),
+                      environment=Environments.INFRA,
+                      blobName=blob_name,
+                      **log_metadata)
+        return False
+    except FileNotFoundError as fnfe:
+        gcl_log_event(logger_name=logger_name,
+                      event_name='Artifact Uploading Error',
+                      message=str(fnfe),
+                      environment=Environments.INFRA,
+                      filePath=file_path,
+                      **log_metadata)
         return False
 
 
