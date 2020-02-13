435e8a9209e311d30dff55471b0a1258044c04cf
shaul <shaul@datascience.co.il>
2020-02-13 12:15:47 +0200

    :wrench: setup.py devel version

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
-import setuptools
+import pathlib
+from setuptools import setup, find_packages
 
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name='dsg-infra',
-    version='1.0.0',
-    author="Or Chen",
-    author_email="or@datascience.co.il",
+setup(
+    name="dsg-infra",
+    version="0.0.0.9001",
+    author="Data Science Group",
+    author_email="or@datascience.co.il, shaul@datascience.co.il, pavels@datascience.co.il",
     description="DSG common infrastructure libraries",
-    long_description=long_description,
+    long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/datascienceisrael/infrastructure",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     classifiers=[
-            "Programming Language :: Python :: 3",
-            "License :: OSI Approved :: MIT License",
-            "Operating System :: OS Independent"
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Operating System :: OS Independent",
     ],
 )
