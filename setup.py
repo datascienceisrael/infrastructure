cc7abfe1a1cb63137832319da1cca6f89bd1c34b
Shaul Abergil <shaul@datascience.co.il>
2020-02-13 08:15:09 +0200

    :wrench: setup.py prepare for 1.0.0

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='infra',
-    version='0.1.0',
+    name='dsg-infra',
+    version='1.0.0',
     author="Or Chen",
     author_email="or@datascience.co.il",
     description="DSG common infrastructure libraries",
